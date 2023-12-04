59bd47b05eeb7d3d414d6a8f8b0a297a943d08a5
Roukanken <kuko0411@gmail.com>
2023-12-04 09:53:39 +0100

    Day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::str::FromStr;
 
 use nom::bytes::complete::{is_a, tag};
@@ -60,7 +60,38 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let (_, cards) = parse_input_by_lines(LotteryCard::parse)(input).unwrap();
+
+    let winning_counts: HashMap<u32, u32> = cards
+        .iter()
+        .map(|card| {
+            let winning: HashSet<u32> = card.winning_numbers.iter().copied().collect();
+            (
+                card.id,
+                card.numbers
+                    .iter()
+                    .filter(|&number| winning.contains(number))
+                    .count() as u32,
+            )
+        })
+        .collect();
+
+    let mut final_copies: HashMap<_, _> = cards.iter().map(|card| (card.id, 1u32)).collect();
+
+    for card in cards {
+        let winning = winning_counts.get(&card.id).copied().unwrap_or(0);
+        let copies = final_copies.get_mut(&card.id).copied().unwrap_or(1);
+
+        for i in 1..=winning {
+            let next_copies = final_copies.remove(&(card.id + i));
+            match next_copies {
+                Some(x) => final_copies.insert(card.id + i, x + copies),
+                None => None,
+            };
+        }
+    }
+
+    Some(final_copies.values().copied().sum())
 }
 
 #[cfg(test)]
@@ -76,6 +107,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(30));
     }
 }
